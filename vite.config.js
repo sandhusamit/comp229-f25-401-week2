3796751228a199e788a6d289c0921951812960e8
Samit Sandhu <ssand139@my.centennialcollege.ca>
2025-09-09 18:01:09 -0400

    Another fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',          // allow external connections
+    port: process.env.PORT,    // use the port Render assigns
+    allowedHosts: ['week2-mkun.onrender.com'] // add your Render host
+  }
 })
